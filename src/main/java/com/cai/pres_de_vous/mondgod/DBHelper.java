82aad5b345051c69332a6ed3094e7ace5d5193b0
Yoann Diquélou <contact@yoann-diquelou.fr>
2015-06-02 15:24:59 +0200

    Start with mongodb

---
+++ src/main/java/com/cai/pres_de_vous/mondgod/DBHelper.java
@@ -0,0 +1,15 @@
+package com.cai.pres_de_vous.mondgod;
+
+import org.vertx.java.platform.Verticle;
+
+/**
+ * Created by Yoann Diquélou on 02/06/15.
+ * Use Mongo DB to singn-in sign-up and check other things in database
+ */
+public class DBHelper extends Verticle{
+
+    @Override
+    public void start() {
+        super.start();
+    }
+}
